8625137ccf9382807b79fbda69607a1bd398722c
Salma <dreambellytb@gmail.com>
2025-10-08 10:51:37 +0200

    ajout de la function playGame

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function getComputerChoice() {
   } else {
     computerChoice = "ciseaux";
   }
-  console.log(computerChoice);
 
   return computerChoice;
 }
@@ -33,27 +32,50 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-  if (
-    (humanChoice.toLowerCase() === "pierre" && computerChoice === "feuille") ||
-    (humanChoice.toLowerCase() === "ciseaux" && computerChoice === "pierre") ||
-    (humanChoice.toLowerCase() === "feuille" && computerChoice === "ciseaux")
-  ) {
-    console.log("Robot win");
-    computerScore++;
-  } else if (
-    (humanChoice.toLowerCase() === "feuille" && computerChoice === "pierre") ||
-    (humanChoice.toLowerCase() === "pierre" && computerChoice === "ciseaux") ||
-    (humanChoice.toLowerCase() === "ciseaux" && computerChoice === "feuille")
-  ) {
-    console.log("Human win");
-    humanScore++;
-  } else {
-    console.log("Egalité");
+function playGame() {
+  function playRound(humanChoice, computerChoice) {
+    if (
+      (humanChoice.toLowerCase() === "pierre" &&
+        computerChoice === "feuille") ||
+      (humanChoice.toLowerCase() === "ciseaux" &&
+        computerChoice === "pierre") ||
+      (humanChoice.toLowerCase() === "feuille" && computerChoice === "ciseaux")
+    ) {
+      console.log("Robot win");
+      computerScore++;
+    } else if (
+      (humanChoice.toLowerCase() === "feuille" &&
+        computerChoice === "pierre") ||
+      (humanChoice.toLowerCase() === "pierre" &&
+        computerChoice === "ciseaux") ||
+      (humanChoice.toLowerCase() === "ciseaux" && computerChoice === "feuille")
+    ) {
+      console.log("Human win");
+      humanScore++;
+    } else {
+      console.log("Egalité");
+    }
   }
+
+  let manche = 0;
+
+  while (manche < 5) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    manche++;
+  }
+
+  if (humanScore < computerScore) {
+    console.log("Le grand gagnant est Computer");
+  } else if (computerScore < humanScore) {
+    console.log("Le grand gagnant est Human ");
+  } else {
+    console.log("EGALITE");
+  }
+  console.log(
+    `Score humain = ${humanScore}, Score Computer = ${computerScore}`
+  );
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
